215de97870c321d8ec02bc27b174ce1c657ca7d3
AdamBabraj <adambabraj@gmail.com>
2022-10-30 15:04:24 +0100

    kryterium wieku

--- apka.py
+++ apka.py
@@ -4,7 +4,6 @@ if wiek.isdigit() == False:
     exit("Wiek musi być liczbą")
 wiek = int(wiek)
 #region = input("Czy jesteś z USA (T - tak, N - nie")
-
 if wiek >= 18:
     print("Witamy!")
 else: print("Żegnamy, przyjdz za ", 18 - wiek, " lat")
